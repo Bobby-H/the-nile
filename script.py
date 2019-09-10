4535d411dc0b65f34c83ee9fa4b473b85793e5f7
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2019-09-09 20:07:11 -0400

    set up test, nile, and script files

---
+++ script.py
@@ -0,0 +1,21 @@
+from nile import get_distance, format_price, SHIPPING_PRICES
+from test import test_function
+
+# Define calculate_shipping_cost() here:
+def calculate_shipping_costs(from_coords, to_coords, shipping_type):
+  pass
+
+# Test the function by calling
+# test_function(calculate_shipping_cost)
+
+# Define calculate_driver_cost() here
+
+
+# Test the function by calling
+# test_function(calculate_driver_cost)
+
+# Define calculate_money_made() here
+
+
+# Test the function by calling
+# test_function(calculate_money_made)
